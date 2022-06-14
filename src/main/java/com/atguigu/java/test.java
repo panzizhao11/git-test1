e5bb44b583f8df79943648e6d421ddec8d0d088d
panzizhao11 <243027795@qq.com>
2022-06-14 16:32:03 +0800

    first commit

---
+++ src/main/java/com/atguigu/java/test.java
@@ -0,0 +1,11 @@
+package com.atguigu.java;
+
+/**
+ * @author shkstart
+ * @create 2022-06-14 16:21
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
